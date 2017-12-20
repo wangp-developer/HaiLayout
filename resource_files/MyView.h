4d663b1699cd116a0a4cb1b44825374b136ab823
Ios Developer <ios_developer@ios-developerdemacbook-pro.local>
2017-12-20 16:07:22 +0800

    d

---
+++ resource_files/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  HaiLayout
+//
+//  Created by Ios_Developer on 2017/12/20.
+//  Copyright © 2017年 hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
